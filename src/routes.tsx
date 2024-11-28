03fbd2d73cc618e56c6ed916408ca846f81b2156
jugunu-21 <aaruhijugunu@gmail.com>
2024-11-28 14:29:07 +0530

    change the file naming

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import SignIn from './pages/signin';
 import SignOut from './pages/signout';
 import { Home } from './pages/home';
-import { SignedOutRequireRoute } from './component/auth/signedOutRequireRoute';
-import { SignedInRequireRoute } from './component/auth/signedInRequireRoute';
+import { SignedOutRequireRoute } from './component/auth/signed-out-require-route';
+import { SignedInRequireRoute } from './component/auth/auth-require-route';
 export default function Routing() {
     return (
         <Router>
