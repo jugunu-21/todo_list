74f5a751114e2447ba395128e92577b90693a9ac
jugunu-21 <aaruhijugunu@gmail.com>
2024-11-28 13:14:50 +0530

    Added SignedInRequireRoute to protect authenticated routes

---
+++ src/component/auth/signedInRequireRoute.tsx
@@ -0,0 +1,17 @@
+
+import { useCurrentUser } from '../../helpers/zustand';
+import getUserSession from '../../helpers/get-current-user';
+import { Navigate } from 'react-router-dom';
+export function SignedInRequireRoute({ children }: { children: React.ReactNode }) {
+    const { currentUser } = useCurrentUser();
+
+    const usersession = getUserSession();
+
+    if (!(currentUser || usersession)) {
+        console.log("No user session detected. Redirecting to /signin.");
+        return <Navigate to="/signin" replace />;
+    }
+
+    console.log("User is signed in:", currentUser || usersession);
+    return <>{children}</>;
+}
