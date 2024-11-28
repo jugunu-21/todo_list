02fddb91557432cea04745fb7c3aadcc1ca5a87b
jugunu-21 <aaruhijugunu@gmail.com>
2024-11-28 20:49:34 +0530

    create a function to generate user's key

---
+++ src/helpers/generateUserKey.ts
@@ -0,0 +1,8 @@
+import { IUser } from "../type/auth";
+function generateUserKey(user: IUser) {
+    if (!user.username || !user.password) {
+        throw new Error("Username and password are required to generate a key.");
+    }
+    return `todos_${user.username}_${user.username}`;
+}
+export { generateUserKey }
