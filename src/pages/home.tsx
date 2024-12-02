44066b389fe87a8a7ea0be5cafb71ad5081799f7
jugunu-21 <aaruhijugunu@gmail.com>
2024-12-02 10:39:22 +0530

    remove error regarding tooltip

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,12 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { Button } from "../components/ui/button"
 import { Input } from "../components/ui/input"
 import { Label } from "../components/ui/label"
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from "../components/ui/tooltip"
+
 import {
     Sheet,
     SheetClose,
